c2980a10c9011fd8bd668e7fd3d9247990629f73
MathMantovan <math123mantovan@gmail.com>
2026-04-08 22:50:31 -0300

    first commit

---
+++ src/BugStore.Api/Models/Customer.cs
@@ -0,0 +1,9 @@
+namespace BugStore.Models;
+public class Customer
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public DateTime BirthDate { get; set; }
+}
